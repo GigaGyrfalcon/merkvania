59a13401e695528a8a34e80141a457e64104cb62
Giga Songulashvili <giga.songulashvili@gmail.com>
2022-04-18 19:39:57 -0400

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mari Erkvania - UX/UI Designer
+
+## UX/UI Designer based in Toronto, Canada.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mari Erkvania - UX/UI Designer</h1>
+      <h2>UX/UI Designer based in Toronto, Canada.</h2>
+      <p>The page is under construction</p>
+    </div>
+  );
+}
+
+export default App;
